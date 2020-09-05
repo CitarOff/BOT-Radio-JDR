18cd214eda08a1c5a1e7d0d3564f5fbf1a47d4af
Citar <61507748+CitarOff@users.noreply.github.com>
2020-09-05 09:45:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,22 +3,10 @@ const config = {
     //Prefix pour donner les commandes au bot
     prefix: "!",
     //https://discordapp.com/developers/applications/
-    idbot: "NjQ1Mjk3NjQyNDg5Nzc0MDgw.XdA5yQ.dNiZR308FsXVeb1YN7FAhk_zjJo",
+    idbot: "",
     //Channel spécialement prévus et dont seulement le MJ a acces ! Attention mettre l'id du channel et non le nom ! 
-    channelmj: "568452353892679681",
+    channelmj: "",
 }
-
-//Pour obtenir l'id d'un channel
-
-//Ouvrir les paramètres discord
-
-//Aller dans apparences 
-
-//Cocher dévelloper mode et quitter les parametres de discord
-
-//Faire clique droit sur le channel dont vous voulez l'id
-
-//Maintenant vous avez l'option qui apparait copier l'id !
 
 //NE PAS TOUCHER 
 
